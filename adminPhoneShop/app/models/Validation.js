2f884d91aa5e77e0ffd62a098652cd8ef9142e5b
Hiep <hiep@HiepnoMacBook-Pro.local>
2022-12-16 18:08:29 +0700

    rs

---
+++ adminPhoneShop/app/models/Validation.js
@@ -0,0 +1,26 @@
+const getEle = (id) => document.getElementById(id);
+class Validations {
+    checkOtion = (idSel, errId, mess) => {
+        if (getEle(idSel).selectedIndex !== 0) {
+            getEle(errId).innerHTML = "";
+            getEle(errId).style.display = "none";
+            return true;
+        }
+        getEle(errId).innerHTML = mess;
+        getEle(errId).style.display = "block";
+        return false;
+    }
+
+    checkNull = (val, errId, mess) => {
+        if (val === "") {
+            getEle(errId).innerHTML = mess;
+            getEle(errId).style.display = "block";
+            return false;
+        }
+        getEle(errId).innerHTML = "";
+        getEle(errId).style.display = "none";
+        return true;
+    }
+}
+
+export default Validations;
